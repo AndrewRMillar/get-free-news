d2884529406a681377876f311539d92f93278da6
Andrew Millar <andrew@millar.dev>
2026-02-01 21:52:12 +0100

    Add logger to the article service with method to get path to log file

--- src/Application/ArticleService.php
+++ src/Application/ArticleService.php
@@ -8,19 +8,34 @@ use Infrastructure\ArticleRepository;
 use Infrastructure\HttpFetcher;
 use Application\ArticleContentExtractor;
 use Domain\Model\Article;
+use Psr\Log\LoggerInterface;
+use Monolog\Logger;
+use Monolog\Level;
+use Monolog\Handler\StreamHandler;
 
 final class ArticleService
 {
+    private LoggerInterface $logger;
+
     public function __construct(
         private HttpFetcher $fetcher,
         private ArticleContentExtractor $extractor,
         private ArticleRepository $repository
-    ) {}
+    ) {
+        $logger = new Logger('article_extractor');
+        $logger->pushHandler(new StreamHandler($this->getLogPath(), Level::Debug));
+
+        $this->logger = $logger;
+    }
 
     public function fetchAndSave(string $url): string|false
     {
+        $this->logger->info('Fetching article', ['url' => $url]);
+        $count = $this->repository->fetchCount();
+
         $html = $this->fetcher->fetch($url);
         if (!$html) {
+            $this->logger->warning('Failed to fetch article', ['url' => $url]);
             return false;
         }
 
@@ -36,6 +51,27 @@ final class ArticleService
 
         $this->repository->save($article);
 
+        $countAfter = $this->repository->fetchCount();
+        if ($countAfter > $count) {
+            $this->logger->info('Article saved successfully', ['url' => $url]);
+        } else {
+            $this->logger->info('Article not saved', ['url' => $url]);
+        }
+
         return $content;
+    }
+
+    public function getLogPath(): string
+    {
+        $logDir = dirname(__DIR__) . '/log';
+
+        if (!is_dir($logDir)) {
+            mkdir($logDir, 0775, true);
+            chmod($logDir, 0775);
+        }
+
+        $logFile = $logDir . '/debug.log';
+
+        return $logFile;
     }
 }
